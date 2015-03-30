042276385dca40356121a8bec50fc37bdb292018
Kitson Kelly <dojo@kitsonkelly.com>
2015-03-30 11:24:19 -0700

    Update tests/unitNodeArray to use jsdom under node

--- tests/unit/NodeArray.ts
+++ tests/unit/NodeArray.ts
@@ -1,24 +1,34 @@
 import assert = require('intern/chai!assert');
 import NodeArray = require('../../NodeArray');
+import dom = require('../../dom');
 import registerSuite = require('intern!object');
+import jsdom = require('dojo/has!host-node?../loadjsdom');
 
 var hasDOM:boolean = typeof document !== 'undefined';
+var doc:Document;
 
 registerSuite({
     name: 'NodeArray',
+    setup: function () {
+        if (!hasDOM) {
+            dom.setDoc(jsdom.jsdom('<html><body></body></html>'));
+        }
+        doc = dom.getDoc();
+    },
+    teardown: function () {
+        dom.resetDoc();
+    },
     'basic': function () {
-        if (!hasDOM) { this.skip('No DOM present'); }
         var nodeArray = new NodeArray();
         assert.equal(nodeArray.length, 0);
-        nodeArray.push(document.createElement('div'));
-        nodeArray.push(document.createElement('p'));
+        nodeArray.push(doc.createElement('div'));
+        nodeArray.push(doc.createElement('p'));
         assert.equal(nodeArray.length, 2);
     },
     '.from()': function () {
-        if (!hasDOM) { this.skip('No DOM present'); }
-        var div = document.createElement('div'),
-            p = document.createElement('p'),
-            span = document.createElement('span');
+        var div = doc.createElement('div'),
+            p = doc.createElement('p'),
+            span = doc.createElement('span');
 
         var nodeArray = NodeArray.from([ div, p, span ]);
         assert.equal(nodeArray.length, 3);
